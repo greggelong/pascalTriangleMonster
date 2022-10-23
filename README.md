bcc9a46f7a5677dbe5e7b759b75b2b4734974384
Greg K <gkreisman_cep@qq.com>
2022-10-23 21:56:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Pascal Triangle Monster : binaryPlot
+
+This is an extension of binary plot.
+
+This is also inspired by wolfram math visualizations 
+
+just in time for Halloween
+
+A binary plot visualizer made in p5.js to plot number sequences, inspired by some plots plots in Wolfram's Mathworld.
+
+It can be adjusted to plot 8 and 16 bit numbers (and others as well)
+I am plotting in binary the natural numbers from 0 to 255. These are 8 bit numbers the leftmost slice is zero and right most is 255. I start counting from the bottom up.
+
+I have plotted the first 255 squares, cubes and triangular numbers too.  
+
+squares and triangular numbers are 16bit and cubes need to be 24 bit so the largest number 16581375 doesn't overflow
+
+A need python hack is to use bit_length
+``` python
+>>> a = 16581375
+>>> a.bit_length()
+24
+
+```
+
+I was also inspired by a talk by Douglas Hofstadter on intertwined patterns of integers and patterns of thought.
+
+#p5js #binary #creativecoding #numbersequences #math #python 
+
+## added Collatz
+
+I have added to plots of the Collatz conjecture, 3n+1
+
+For all numbers 1 to 255  (starts at one for the collatz plots)
+
+### I have plotted the path length:  
+
+for example:
+- from 1 to 1 takes 0 steps so a length of 0
+-  from  2 to 1 one takes 1 steps so a length of 1
+-  from 3 to 1 takes 7 steps [3, 10, 5, 16, 8, 4, 2, 1] so a length of 7
+-  the largest number of steps to one for numbers up to 255 is 127 so I am using 8 bit numbers
+
+### I have also plotted sum of the path (including 1)
+  for example the sum for 3 will be 49 
+  
+  [3, 10, 5, 16, 8, 4, 2, 1] = 49
+
+live at: https://greggelong.github.io/binaryPlot/
+
+
+
+
+
---
+++ sketch.js
@@ -0,0 +1,100 @@
+
+// some comments are for a different version of 
+
+// get the height from the bitlen let H = 25; // 400 height divided by 8  400/8
+let W; // 1536 divided by 256
+let rr = 1
+let pascal2 = [
+  [0, 0],
+  [0, 1, 0]
+]; // a few rows of the
+
+function newRow(oldRow) {
+  let new_row = [];
+  for (let i = 0; i < oldRow.length - 1; i++) {
+    new_row[i] = oldRow[i] + oldRow[i + 1];  // the new row is the sum of neighbors of the old row
+
+  }
+  new_row.push(0);  // put a zero on the back
+  new_row.unshift(0); // put a zero on the front
+
+  return new_row;  
+}
+
+function makeTriangle(n) {
+
+  for (let i = 1; i < n; i++) { // need to start with the second index to form the triangle
+    pascal2[i + 1] = newRow(pascal2[i]);
+
+  }
+
+}
+
+function setup() {
+  createCanvas(800, 800);
+  
+  background(0);  // don't need this
+  makeTriangle(22)
+  console.log(pascal2)
+  textSize(15)
+  mouseReleased();
+
+}
+
+function mouseReleased(){
+  background(0)
+  W=40
+  drawN(rr%20)
+  fill(0,255,0)
+  text(rr%20+":: "+pascal2[rr%20], 10,770);
+  rr++
+}
+
+function drawN(r){
+  
+  let x =0;
+  for (let i = 0; i < pascal2[r].length; i++) {          // 
+    binSlice(x,pascal2[r][i], 16);
+    x+=W; // move the x pos by width set global W
+  }
+  
+  
+  
+}
+
+
+
+function binConvert(a, bitLen) {
+  // takes in a decimal and a bit length and returns a list of ones and zeros binary for that number
+
+  let b = a.toString(2); // converts it to binary but leading zeros, not 8 bits eg. 3 = "11"
+  let mask = "0".repeat(bitLen); // a mask to get the extra zeros
+  let c = mask.slice(0, bitLen - b.length); // slice to get the right number of zeros
+  // eg. if b = "11" then c = "000000"
+  let binstring = c + b; // binary string so 3 will give 00000011 8 bits
+
+  let binArray = int(binstring.split("")); // is an aray of ints so [0,0,0,0,0,0,1,1]
+  return binArray;
+}
+
+function binSlice(x, Dnum, bitLen) {
+  // takes an x position, decimal number, and a bitLen of the largest number in the sequence
+  let H = 40 //Math.floor(height/bitLen); // need to floor this for collatz path sum which has a bit length of 17
+  // takes in a x location on canvas and a decimal number
+  // calls the convert function
+  let binA = binConvert(Dnum, bitLen);
+  // only need an x because y is determined by list length and size
+  let y = 0;
+  for (const element of binA) {
+    if (element == 0) {
+      fill(0);
+      rect(x, y, W,H);
+    } else {
+      fill(0, 255, 0);
+      rect(x, y, W, H);
+    }
+    y += H;
+  }
+}
+
+
